cf1d6c9b8d3846cabd7241d6b95e714dbadbb8fe
Justin Traglia <jtraglia@pm.me>
2025-05-27 09:35:46 -0500

    Update version to v0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="ethspecify",
-    version="0.1.4",
+    version="0.2.0",
     description="A utility for processing Ethereum specification tags.",
     long_description=long_description,
     long_description_content_type="text/markdown",
